842b1528ccd3aef4e9eae1acc40a21f0a46e5177
Sweyd Manaf <abdulsweyd@gmail.com>
2023-02-26 20:13:33 +0200

    Update script.js

--- 04/script.js
+++ 04/script.js
@@ -1,38 +1,39 @@
-const divMessages = document.querySelector('.messages')
+const divMessages = document.querySelector(".messages");
 const messages = document.querySelector(".messages");
 messages.lastElementChild.scrollIntoView();
-
 
 function closeApp() {
   window.close();
 }
 
-function getHoraAtual(){
-    const dataAtual = new Date()
-    const hora = dataAtual.getHours().toString().padStart(2, '0')
-    const minutos = dataAtual.getMinutes().toString().padStart(2, '0')
-    return hora + ':' + minutos
+function getHoraAtual() {
+  const dataAtual = new Date();
+  const hora = dataAtual.getHours().toString().padStart(2, "0");
+  const minutos = dataAtual.getMinutes().toString().padStart(2, "0");
+  return hora + ":" + minutos;
 }
 
-function sendMessage(){
-    let message = document.querySelector('#text-message').value
-    let data = Date()
-    
-    if (message !== '') {
-        let divMessage = document.createElement('div')
-        divMessage.classList.add('message')
-        divMessage.classList.add('you')
+function sendMessage() {
+  let message = document.querySelector("#text-message").value;
+  let data = Date();
 
-        let divTop = document.createElement('div')
-        divTop.classList.add('top')
-        divTop.innerText = `Você - ${getHoraAtual()}`
+  if (message !== "") {
+    let divMessage = document.createElement("div");
+    divMessage.classList.add("message");
+    divMessage.classList.add("you");
 
-        let divBody = document.createElement('div')
-        divBody.classList.add('body')
-        divBody.innerText = message
+    let divTop = document.createElement("div");
+    divTop.classList.add("top");
+    divTop.innerText = `Você - ${getHoraAtual()}`;
 
-        divMessage.appendChild(divTop)
-        divMessage.appendChild(divBody)
-        divMessages.appendChild(divMessage)
-    }
+    let divBody = document.createElement("div");
+    divBody.classList.add("body");
+    divBody.innerText = message;
+
+    divMessage.appendChild(divTop);
+    divMessage.appendChild(divBody);
+    divMessages.appendChild(divMessage);
+  }
+
+  message = "";
 }
