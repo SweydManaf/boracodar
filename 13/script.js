d4cd2ab9a2b762410114d0ad089ec0db8ffccfe1
Sweyd Abdul Manaf <abdulsweyd@gmail.com>
2023-04-08 10:03:20 +0200

    melhorando o projeto

--- 13/script.js
+++ 13/script.js
@@ -6,17 +6,17 @@ IMask(document.querySelector("#cc-number"), {
   mask: "0000 0000 0000 0000",
 })
 
-IMask(document.querySelector("#cc-validty"), {
+IMask(document.querySelector("#cc-validity"), {
   mask: "MM{/}YY",
   blocks: {
     MM: {
-      mask: iMask.MaskedRange,
+      mask: IMask.MaskedRange,
       from: 1,
       to: 12,
     },
     YY: {
-      mask: iMask.MaskedRange,
-      from: String(new Date().getFullYear() - 10).slice(2),
+      mask: IMask.MaskedRange,
+      from: String(new Date().getFullYear()).slice(2),
       to: String(new Date().getFullYear() + 10).slice(2),
     },
   },
